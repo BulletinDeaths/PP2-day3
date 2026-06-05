8fcc0787d942d3725f216cbf93119e01036c19d1
BulletinDeaths <vika021107777@gmail.com>
2026-06-05 15:08:36 +0300

    Delete onlineschool directory

--- onlineschool/models/student.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, student_id: int, name: str, email: str):
-        self.id = student_id
-        self.name = name
-        self.email = email
-        self.history = []
--- onlineschool/models/teacher.py
+++
@@ -1,5 +0,0 @@
-class Teacher:
-    def __init__(self, teacher_id: int, name: str, specialization: str):
-        self.id = teacher_id
-        self.name = name
-        self.specialization = specialization
